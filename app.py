e2738a1c3d1271421d6ced920c636bae5f789ba0
Newsstate <newsstate6294@gmail.com>
2025-07-17 14:11:21 +0530

    cors update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # ← ADD THIS
 
 app = Flask(__name__)
+CORS(app)  # ← ENABLE CORS FOR ALL DOMAINS
 
 @app.route('/')
 def home():
